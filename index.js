b5779e6291c61e43312714d47c17eeae6ea70726
Brandon Nestor Reis <brandon.nestor.reis@gmail.com>
2023-08-18 13:18:57 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0, 2)
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier
+    }
+  }
+
+const fareDoubler = createFareMultiplier(2)
+//console.log(fareDoubler(20))
+
+const fareTripler = createFareMultiplier(3)
+//console.log(fareTripler(20))
+
+function selectDifferentDrivers(drivers, x) {
+    return x(drivers)
+}
+
+//console.log(selectDifferentDrivers(['Luis', 'Brandon', 'Bob', 'Jill'], returnLastTwoDrivers))
+
